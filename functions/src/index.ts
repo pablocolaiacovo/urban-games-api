a9ae04dbf734c63c1f8860ebb8636f86087e4f76
Pablo Colaiacovo <pablo.colaiacovo@gmail.com>
2018-07-25 23:58:07 -0300

    Initial firebase functions restart

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+app.use(cors({ origin: true }));
+// https://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
+
+app.get("/", async (req: express.Request, res: express.Response) => {
+  res.send("Hello from Firebase!");
+});
+
+app.get("/users", async (req: express.Request, res: express.Response) => {
+  res.json([{ name: "Pablo Colaiacovo", email: "pablo.colaiacovo@gmail.com" }]);
+});
+
+app.get("*", async (req: express.Request, res: express.Response) => {
+  res.status(404).send("This route does not exist.");
+});
+
+export const api = functions.https.onRequest(app);
